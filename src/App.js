fcea0390b938a9e66b867e737fb5e1a2ccb33986
kajak7 <37246524+kajak7@users.noreply.github.com>
2018-08-20 16:54:48 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import UsersList from './UsersList'
 
+// z braku laku istniających userów definiuję po prostu jako stałą
+// w prawdziwej aplikacji pochodziliby z API i/lub byli w Reduksowym storze 
 const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
 
 class App extends React.Component {
   constructor() {
-    super();    
-    
+    super();
+
     this.state = {
       filteredUsers: allUsers
     };
   }
 
-  filterUsers(e) {
+  filterUsers = (e) => {
     const text = e.currentTarget.value;
     const filteredUsers = this.getFilteredUsersForText(text)
     this.setState({
       filteredUsers
     })
   }
-  
+
   getFilteredUsersForText(text) {
     return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
   }
-  
-  render () {
+
+  render() {
     return (
       <div>
-        <input onInput={this.filterUsers.bind(this)} />
+        <input onInput={this.filterUsers} />
         <UsersList users={this.state.filteredUsers} />
       </div>
     );
   }
 };
 
-const UsersList = ({ users }) => {
-  if (users.length > 0) {
-    return (
-      <ul>
-        {users.map(user => <li key={user}>{user}</li>)}
-      </ul>
-    );
-  }
-
-  return (
-    <p>No results!</p>
-  );
-};
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('includes input', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<input />)).toEqual(true)
+});
+
+
+it('includes UsersList', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<UsersList />)).toEqual(true)
 });
